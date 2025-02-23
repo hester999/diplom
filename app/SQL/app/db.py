a9fa13ffdca5ea76a86666bf07dacd5cdb5a0fc4
hester999 <hester@MacBook-Air-Ivan-3.local>
2025-02-23 20:27:48 +0300

    fix db

--- app/SQL/app/db.py
+++ app/SQL/app/db.py
@@ -3,11 +3,5 @@ from flask import current_app
 
 def get_db():
     """Подключение к базе данных PostgreSQL"""
-    conn = psycopg2.connect(
-        dbname=current_app.config['DATABASE'],
-        user=current_app.config['USER'],
-        password=current_app.config['PASSWORD'],
-        host=current_app.config['HOST'],
-        port=current_app.config['PORT']
-    )
+    conn = psycopg2.connect(current_app.config['DATABASE_URL'])
     return conn
--- app/SQL/app/main.py
+++ app/SQL/app/main.py
@@ -1,15 +1,20 @@
 from flask import Flask, send_from_directory
 from routes import sql_injection_bp  # Изменен путь на относительный
-
+import os
 app = Flask(__name__, static_folder='../static', template_folder='../templates')
 
 
 # Конфигурация приложения
-app.config['DATABASE'] = 'vulnerable_db'
-app.config['USER'] = 'admin'
-app.config['PASSWORD'] = '123'
-app.config['HOST'] = 'localhost'
-app.config['PORT'] = '5432'
+# app.config['DATABASE'] = 'vulnerable_db'
+# app.config['USER'] = 'admin'
+# app.config['PASSWORD'] = '123'
+# app.config['HOST'] = 'localhost'
+# app.config['PORT'] = '5432'
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://admin:123@localhost:5432/vulnerable_db')
+
+# Сохранение в конфигурации Flask
+app.config['DATABASE_URL'] = DATABASE_URL
 
 # Регистрация blueprint с маршрутом для SQL инъекций
 app.register_blueprint(sql_injection_bp)
